ae45f79c43e93b93bc41ed1d420236a2410d8703
Dan Huang <jhyqt5@gmail.com>
2016-06-09 11:22:08 -0600

    feat: Add outlets and actions

--- ColorSwitches/ViewController.swift
+++ ColorSwitches/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBAction func changeToColor(sender: UISwitch) {
+        if sender.tag == 0 {
+            print("red")
+        } else if sender.tag == 1 {
+            print("green")
+        } else {
+            print("blue")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let switches = [redSwitch, greenSwitch, blueSwitch]
+        
+        for switchButton in switches {
+            switchButton.on = false
+        }
     }
-
-
+    
 }
 
